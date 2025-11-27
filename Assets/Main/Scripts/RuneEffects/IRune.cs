7d19d1682f51d2adf5ad8e69c58b0f771e6a84f7
JessicaTheEm2 <a24chrca@student.his.se>
2025-11-27 11:48:29 +0100

    e

--- Assets/Main/Scripts/RuneEffects/IRune.cs
+++
@@ -1,9 +0,0 @@
-public interface IRune
-{
-    int ValueID { get; }
-
-    void Drop();
-    void OnInteract();
-    void PickUp();
-    void TriggerPillarPlacement(int itemIndex, Alter[] pillars);
-}
