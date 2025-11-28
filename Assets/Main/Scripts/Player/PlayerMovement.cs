1f92e77d6131eedd7ddc30d1b3d7943669d9c7c3
EnderMenderMan <killerendermenderman@gmail.com>
2025-11-28 14:59:11 +0100

    Player Animations

--- Assets/Main/Scripts/Player/PlayerMovement.cs
+++ Assets/Main/Scripts/Player/PlayerMovement.cs
@@ -3,17 +3,19 @@ using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Vector2 dir;
 
     private Rigidbody2D rb;
+    Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -27,10 +29,25 @@ public class PlayerMovement : MonoBehaviour
     {
         PlayerInteract.InputActions.Player.Move.performed += context =>
         {
+            animator.SetBool("IsMoving", true);
             dir = context.ReadValue<Vector2>();
+
+            if (dir.x != 0)
+                animator.SetInteger("DirState", 0);
+            else if (dir.y > 0)
+                animator.SetInteger("DirState", 1);
+            else
+                animator.SetInteger("DirState", -1);
+
+
+            Vector3 newRotation = new Vector3(transform.rotation.x, 0, transform.rotation.z);
+            if (dir.x > 0)
+                newRotation.y = 180;
+            transform.eulerAngles = newRotation;
         };
         PlayerInteract.InputActions.Player.Move.canceled += context =>
         {
+            animator.SetBool("IsMoving", false);
             dir = Vector2.zero;
         };
         PlayerInteract.InputActions.Player.Move.Enable();
