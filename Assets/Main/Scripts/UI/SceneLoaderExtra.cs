a6a10bbad7d3feca2120e9f0ea63914f9a98d29c
JessicaTheEm2 <a24chrca@student.his.se>
2025-12-11 14:38:15 +0100

    ;enu stuff + J button stuff

--- Assets/Main/Scripts/UI/SceneLoaderExtra.cs
+++ Assets/Main/Scripts/UI/SceneLoaderExtra.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 using UnityEngine.Events;
 public class SceneLoaderExtra : MonoBehaviour
 {
-    [Tooltip("When you press th escape button")] public UnityEvent OnESCPressed;
+    [Tooltip("When you press the escape button")] public UnityEvent OnESCPressed;
+    [Tooltip("When you press the J button")] public UnityEvent OnJPressed;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) 
             OnESCPressed.Invoke();
+        if (Input.GetKeyDown(KeyCode.J))
+            OnJPressed.Invoke();
     }
 
     //public void ContinuesGame(MenuScritablleObject menuScritablleObject) { MenuScritablleObject.continueGame = true; }
