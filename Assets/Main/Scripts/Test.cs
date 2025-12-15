4f2293fea76d7565c67cdb1aad31c01d33514044
EnderMenderMan <killerendermenderman@gmail.com>
2025-12-15 10:34:25 +0100

    Test Shader Stuff

--- Assets/Main/Scripts/Test.cs
+++ Assets/Main/Scripts/Test.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] LayerMask layerMask;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -13,6 +14,11 @@ public class Test : MonoBehaviour
     public void TestLog(string input = "TEST")
     {
         Debug.Log(input);
+    }
+    public void TestLayerMask(int layerCheck)
+    {
+        if (((1 << layerCheck) & layerMask) > 0)
+            Debug.Log("YES");
     }
 
     public void LoadScene(int buildIndex)
